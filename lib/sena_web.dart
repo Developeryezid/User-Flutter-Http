fe6fc6cc8b769b48a936a31711cc25012a9c98b6
Yeperez <110582390+Developeryezid@users.noreply.github.com>
2022-11-16 02:58:16 -0500

    first commit

---
+++ lib/sena_web.dart
@@ -0,0 +1,17 @@
+import 'dart:convert' as convert;
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'My app',
+        home: Scaffold(
+            appBar: AppBar(title: Text('Ingreso de registro')),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            )));
+  }
+}
